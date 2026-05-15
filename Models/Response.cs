afbb1b601c3af95c5590bef12dfb35cc39ce6d87
NABARUN <nbiswas100@outlook.com>
2026-05-15 21:38:52 +0530

    Changes in model classes, usercontroller, program file and added connection string

--- Models/Response.cs
+++ Models/Response.cs
@@ -4,7 +4,7 @@ public class Response
 {
     public int StatusCode { get; set; }
 
-    public string StatusMessage { get; set; }
+    public string? StatusMessage { get; set; }
     
     public List<Users> listUsers { get; set; }
 
--- Models/Users.cs
+++ Models/Users.cs
@@ -3,29 +3,20 @@ namespace PharmaNestBackend.Models;
 public class Users
 {
     public int ID { get; set; }
-    
-    public string FirstName { get; set; }
 
-    public string LastName { get; set; }
+    public string? FirstName { get; set; }
 
-    public string Password { get; set; }
+    public string? LastName { get; set; }
 
-    public string Email { get; set; }
+    public string? Password { get; set; }
+
+    public string? Email { get; set; }
 
     public decimal Fund { get; set; }
 
-    public string Type { get; set; }
+    public string? Type { get; set; }
 
-    public int Status { get; set; }
+    public string? Status { get; set; }
 
     public DateTime CreatedOn { get; set; }
 }
-
-
-
-
-
-
-
-
-
